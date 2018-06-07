6604bc705705c006058e3260da65e572dc968623
masaru.yamaguchi <ec2-user@ip-172-31-11-70.us-east-2.compute.internal>
2018-06-07 04:58:07 +0000

    post1

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,13 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 
 
-Route::get('/', function () {
-    return view('welcome');
 
 
- });
+ 
+Route::get('/', 'MicropostsController@index');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
+    
 });
